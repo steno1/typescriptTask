a7f16862a8a661312b0eee944329cc3c448d97fe
steno1 <onuprinceley@gmail.com>
2025-02-19 17:37:49 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,59 @@
+interface User {
+    type: 'user';
+    name: string;
+    age: number;
+    occupation: string;
+}
+
+interface Admin {
+    type: 'admin';
+    name: string;
+    age: number;
+    role: string;
+}
+
+export type Person = User | Admin;
+
+export const persons: Person[] = [
+    { type: 'user', name: 'Max Mustermann', age: 25, occupation: 'Chimney sweep' },
+    { type: 'admin', name: 'Jane Doe', age: 32, role: 'Administrator' },
+    { type: 'user', name: 'Kate Müller', age: 23, occupation: 'Astronaut' },
+    { type: 'admin', name: 'Bruce Willis', age: 64, role: 'World saver' },
+    { type: 'user', name: 'Wilson', age: 23, occupation: 'Ball' },
+    { type: 'admin', name: 'Agent Smith', age: 23, role: 'Anti-virus engineer' }
+];
+
+export function logPerson(person: Person) {
+    console.log(
+        ` - ${person.name}, ${person.age}, ${person.type === 'admin' ? person.role : person.occupation}`
+    );
+}
+
+// Utility type to remove "type" property from User and Admin
+type FilterCriteria<T extends Person> = Partial<Omit<T, 'type'>>;
+
+// Function with improved typings
+export function filterPersons<T extends Person>(
+    persons: Person[],
+    personType: T['type'],
+    criteria: FilterCriteria<T>
+): T[] {
+    return persons
+        .filter((person): person is T => person.type === personType)
+        .filter((person) =>
+            Object.entries(criteria).every(([key, value]) =>
+                (person as any)[key] === value
+            )
+        );
+}
+
+export const usersOfAge23 = filterPersons(persons, 'user', { age: 23 });
+export const adminsOfAge23 = filterPersons(persons, 'admin', { age: 23 });
+
+console.log('Users of age 23:');
+usersOfAge23.forEach(logPerson);
+
+console.log();
+
+console.log('Admins of age 23:');
+adminsOfAge23.forEach(logPerson);
